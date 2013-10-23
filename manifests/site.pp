fae10bea7881f26d9fdb8975a10f72a7aeb93bd2
jake <bigjake2s@gmail.com>
2013-10-23 15:56:47 -0400

    Added prereqs from vagrant manifest to keep everything up to date

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,15 @@
+include coverage::puppet
+
+package { 'etckeeper':
+  ensure => latest,
+}
+
+package { 'ntpdate':
+  ensure => latest,
+}
+
+class { 'resolver':
+  dns_servers => ['192.168.20.34'],
+}
+
 import 'nodes/*.pp'
