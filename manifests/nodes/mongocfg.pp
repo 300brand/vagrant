8ead7c88f129a68c93cc61b99d1d809e5672069f
jake <bigjake2s@gmail.com>
2013-10-25 15:18:17 -0400

    Added gearman configuration to mongocfg personalities

--- manifests/nodes/mongocfg.pp
+++ manifests/nodes/mongocfg.pp
@@ -1,4 +1,9 @@
 node /^\w+\.mongocfg\.\w+\.coverage\.net/ {
   include coverage::mongo::config
   include coverage::mongo::shell
+
+  class { 'gearman':
+    listen                => $ipaddress_eth1,
+    disable_limits_module => true,
+  }
 }
