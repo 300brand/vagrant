88e281173a65dee3941a7c914c3a898678da4a2b
jake <bigjake2s@gmail.com>
2013-10-25 17:50:10 -0400

    Defined Mongo nodes and included goodies

--- manifests/nodes/mongo.pp
+++ manifests/nodes/mongo.pp
@@ -1,82 +1,34 @@
 # +--------------+
-# | CAMPBELTOWN  |
-# | longrow      | ----------[rs0]
-# | springbank   | ------[rs1] |
-# +--------------+         |   |
-# | HIGHLAND     |         |   |
-# | glenmorangie | --[rs2] |   |
-# | themacallan  | ----|---|---+
-# +--------------+     |   |
-# | ISLAND       |     |   |
-# | highlandpark | ----|---+
-# | lediag       | ----+
+# | HIGHLAND     |
+# | glenmorangie | --[rs0]
+# | thedalmore   | ----+--[rs1]
+# | dalwhinnie   | ----+----+--[rs2]
+# | themacallan  | ----+----+----+--[rs3]
+# +--------------+     |    |    |    |
+# | ISLAND       |     |    |    |    |
+# | lediag       | ----+    |    |    |
+# | highlandpark | ---------+    |    |
+# | talisker     | --------------+    |
+# | tobermory    | -------------------+
 # +--------------+
 
-node 'longrow.mongo.campbeltown.coverage.net' {
+node 'glenmorangie.mongo.highland.coverage.net', 'lediag.mongo.island.coverage.net' {
   class { 'coverage::mongo::data':
     replSet => 'rs0',
   }
-
-
-
-  # file { 'mongodir':
-  #   ensure => directory,
-  #   group  => mongodb,
-  #   mode   => '0755',
-  #   owner  => mongodb,
-  #   path   => "/vagrant/${::fqdn}/mongocfg",
-  #   recurse => true,
-  # }
-
-  # class { 'mongodb':
-  #   options  => {
-  #     replSet => 'rs0',
-  #   },
-  #   template => 'coverage/mongod.conf.erb',
-  # }
 }
-
-# node 'springbank.mongo.campbeltown.coverage.net' {
-#   class { 'mongodb':
-#     template => 'coverage/mongod.conf.erb',
-#     options  => {
-#       replSet => 'rs1',
-#     }
-#   }
-# }
-
-# node 'glenmorangie.mongo.highland.coverage.net' {
-#   class { 'mongodb':
-#     template => 'coverage/mongod.conf.erb',
-#     options  => {
-#       replSet => 'rs2',
-#     }
-#   }
-# }
-
-# node 'themacallan.mongo.highland.coverage.net' {
-#   class { 'mongodb':
-#     template => 'coverage/mongod.conf.erb',
-#     options  => {
-#       replSet => 'rs0',
-#     }
-#   }
-# }
-
-# node 'highlandpark.mongo.island.coverage.net' {
-#   class { 'mongodb':
-#     template => 'coverage/mongod.conf.erb',
-#     options  => {
-#       replSet => 'rs1',
-#     }
-#   }
-# }
-
-# node 'ledaig.mongo.island.coverage.net' {
-#   class { 'mongodb':
-#     template => 'coverage/mongod.conf.erb',
-#     options  => {
-#       replSet => 'rs2',
-#     }
-#   }
-# }
+node 'thedalmore.mongo.highland.coverage.net', 'highlandpark.mongo.island.coverage.net' {
+  class { 'coverage::mongo::data':
+    replSet => 'rs1',
+  }
+}
+node 'dalwhinnie.mongo.highland.coverage.net', 'talisker.mongo.island.coverage.net' {
+  class { 'coverage::mongo::data':
+    replSet => 'rs2',
+  }
+}
+node 'themacallan.mongo.highland.coverage.net', 'tobermory.mongo.island.coverage.net' {
+  class { 'coverage::mongo::data':
+    replSet => 'rs3',
+  }
+}
