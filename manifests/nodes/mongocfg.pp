7fe3c39121a72bc3bbe2e7d433dfafacc9e49bb9
jake <bigjake2s@gmail.com>
2013-10-28 15:12:35 -0400

    Attempting to install gearman via apt-testing repo

--- manifests/nodes/mongocfg.pp
+++ manifests/nodes/mongocfg.pp
@@ -2,9 +2,16 @@ node /^\w+\.mongocfg\.\w+\.coverage\.net/ {
   include coverage::mongo::config
   include coverage::mongo::shell
 
+  apt::repository { 'testing':
+    url        => 'http://mirrors.kernel.org/debian/',
+    distro     => 'testing',
+    repository => 'main',
+  }
+
   class { 'gearman':
     listen                => $ipaddress_eth1,
     disable_limits_module => true,
     require_epel          => [], # circumvents the undef issues in module
+    require               => Apt::Repository['testing'],
   }
 }
