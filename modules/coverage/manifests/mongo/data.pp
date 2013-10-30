320cef1b43f95e7fafb4859fef3432b4888d218c
jake <bigjake2s@gmail.com>
2013-10-30 10:16:16 -0400

    Added port as an option; defaulting to 27017

--- modules/coverage/manifests/mongo/data.pp
+++ modules/coverage/manifests/mongo/data.pp
@@ -2,7 +2,10 @@
 #
 # Configures a mongod instance running in configsvr mode
 #
-class coverage::mongo::data ($replSet = undef) {
+class coverage::mongo::data (
+  $replSet = undef,
+  $port    = 27017,
+  ) {
   if $replSet == undef {
     fail('No replSet defined')
   }
@@ -14,6 +17,7 @@ class coverage::mongo::data ($replSet = undef) {
     template  => 'coverage/mongod.conf.erb',
     options   => {
       replSet => $replSet,
+      port    => $port,
     },
     require   => Fstab['mongo_storage'],
   }
