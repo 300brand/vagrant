b57622bba7a0de9aae4ecf55a08fbad7d660c981
jake <bigjake2s@gmail.com>
2013-11-06 13:19:27 -0500

    Setting up higher limits for file openings and such

--- manifests/nodes/mongo.pp
+++ manifests/nodes/mongo.pp
@@ -16,19 +16,83 @@ node 'glenmorangie.mongo.highland.coverage.net', 'ledaig.mongo.island.coverage.n
   class { 'coverage::mongo::data':
     replSet => 'rs0',
   }
+
+  limits::limits {
+    'mongodb_nofile':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nofile',
+      both       => 64000,
+      notify     => Service['mongodb'];
+    'mongodb_nproc':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nproc',
+      both       => 32000,
+      notify     => Service['mongodb'];
+  }
 }
+
 node 'thedalmore.mongo.highland.coverage.net', 'highlandpark.mongo.island.coverage.net' {
   class { 'coverage::mongo::data':
     replSet => 'rs1',
   }
+
+  limits::limits {
+    'mongodb_nofile':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nofile',
+      both       => 64000,
+      notify     => Service['mongodb'];
+    'mongodb_nproc':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nproc',
+      both       => 32000,
+      notify     => Service['mongodb'];
+  }
 }
+
 node 'dalwhinnie.mongo.highland.coverage.net', 'isleofjura.mongo.island.coverage.net' {
   class { 'coverage::mongo::data':
     replSet => 'rs2',
   }
+
+  limits::limits {
+    'mongodb_nofile':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nofile',
+      both       => 64000,
+      notify     => Service['mongodb'];
+    'mongodb_nproc':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nproc',
+      both       => 32000,
+      notify     => Service['mongodb'];
+  }
 }
+
 node 'themacallan.mongo.highland.coverage.net', 'tobermory.mongo.island.coverage.net' {
   class { 'coverage::mongo::data':
     replSet => 'rs3',
   }
+
+  limits::limits {
+    'mongodb_nofile':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nofile',
+      both       => 64000,
+      notify     => Service['mongodb'];
+    'mongodb_nproc':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nproc',
+      both       => 32000,
+      notify     => Service['mongodb'];
+  }
 }
+
--- manifests/nodes/node.pp
+++ manifests/nodes/node.pp
@@ -2,4 +2,19 @@ node /^\w+\.node\.\w+\.coverage\.net/ {
   include coverage::golang
   include coverage::service
   include coverage::mongo::shell
+
+  limits::limits {
+    'mongodb_nofile':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nofile',
+      both       => 64000,
+      notify     => Service['mongodb'];
+    'mongodb_nproc':
+      ensure     => present,
+      user       => 'mongodb',
+      limit_type => 'nproc',
+      both       => 32000,
+      notify     => Service['mongodb'];
+  }
 }
