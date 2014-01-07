f2762d45c4555b66c3ea4da819319783c761f331
jake <bigjake2s@gmail.com>
2014-01-07 15:38:44 -0500

    Fixed golang installation now that 1.2 is in apt

--- modules/coverage/manifests/golang.pp
+++ modules/coverage/manifests/golang.pp
@@ -11,28 +11,6 @@ class coverage::golang {
     'mercurial':
       ensure => present;
     'golang-go':
-      ensure => absent;
-    'golang-src':
-      ensure => absent;
-  }
-
-  exec {
-    'install_old_go':
-      command     => '/usr/bin/apt-get install -y golang-go golang-src',
-      creates     => '/usr/bin/go',
-      environment => ['DEBIAN_FRONTEND=noninteractive'],
-      notify      => Exec['godeb'];
-    'godeb':
-      command     => '/usr/bin/go get launchpad.net/godeb',
-      environment => ['GOPATH=/usr/share/go'],
-      creates     => '/usr/share/go/bin/godeb',
-      notify      => [
-        Exec['install_go'],
-        Package['golang-go'],
-        Package['golang-src'],
-      ];
-    'install_go':
-      command => '/usr/share/go/bin/godeb install 1.1.2',
-      creates => '/usr/bin/go';
+      ensure => present;
   }
 }
